26702d2b2d7eb1b25f1d5868893b5298e604bd73
letunglam001 <letunglam001@gmail.com>
2021-08-12 16:55:27 +0700

    Update relationship

--- server/src/main/java/web/model/Book.java
+++ server/src/main/java/web/model/Book.java
@@ -1,18 +1,19 @@
 package web.model;
 
+
+
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import javax.persistence.Table;
 import javax.validation.constraints.Max;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @Entity
-@NoArgsConstructor
 @Table(name = "book")
 public class Book {
 	@Id
@@ -23,4 +24,53 @@ public class Book {
 	@Max(100)
 	private String author;
 	private String category;
+	@ManyToOne
+	private User user;
+	
+	
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Book(int id, @Max(100) String title, @Max(100) String author, String category) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.category = category;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+	
 }
--- server/src/main/java/web/model/User.java
+++ server/src/main/java/web/model/User.java
@@ -1,5 +1,6 @@
 package web.model;
 import java.sql.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,8 +13,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 
-import lombok.*;
-@Data
+
 @Entity
 @Table(name = "user")
 public class User {
@@ -30,6 +30,8 @@ public class User {
 	@Min(1)
 	@Max(120)
 	private int age;
+	@OneToMany(mappedBy = "user")
+	private List<Book> books;
 	
 	public User() {
 		super();
@@ -46,8 +48,65 @@ public class User {
 		Date now = new Date(System.currentTimeMillis());
 		this.age = now.getYear()- dob.getYear();
 	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
 
 
 	
+
 	
 }
